48bf66bf279ad12c5c70dd26bf412fca57d28502
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-04 09:10:50 +0000

    [Add]Update action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to '/books'
+  end
+
+
   private
     def book_params
       params.require(:book).permit(:title,:body)
