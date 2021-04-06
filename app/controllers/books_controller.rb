a75d8577e0588cd84db40dc884096cb0839dc2dd
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-06 05:15:23 +0000

    [Update]全リダイレクト先完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book)
   end
 
   def edit
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def update
     book =Book.find(params[:id])
     book.update(book_params)
-    redirect_to show
+    redirect_to book_path(book)
   end
 
   def destory
