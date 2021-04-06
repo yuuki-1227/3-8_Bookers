2f354f6c8353f30355ebcf59b2f6a7794721ec2c
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-06 06:20:46 +0000

    [Update]destroyアクションの正常動作

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: 'Book was successfully created'
   end
 
   def edit
@@ -28,12 +28,13 @@ class BooksController < ApplicationController
   def update
     book =Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: 'Book was successfully updated'
   end
 
-  def destory
-    blog = Blog.find(params[:id])
-    blog.destroy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books', notice: 'Book was successfully destroyed'
   end
 
 
